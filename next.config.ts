7198c74883fb3ee3833d3b221a4a5d967be28af0
Joan Xeco <samtext@example.com>
2026-08-04 16:09:19 +0300

    Added rewrite rules to forward /api/* to backend monolith

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://xecoflow-2gen.onrender.com/v1/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
